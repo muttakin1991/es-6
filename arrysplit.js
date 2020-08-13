300f4b38f722ff589bfdddfa618f374e9aadc0b6
muttakin1991 <shawonmuttakin@gmail.com>
2020-08-13 11:29:58 -0700

    final comments of this module

---
+++ arrysplit.js
@@ -0,0 +1,20 @@
+// const nums = [1,2,3,4,5,6,7,8,9];
+// const part = nums.slice(2, 5);
+// console.log(part);
+// console.log(nums);
+
+// const nums = [1,2,3,4,5,6,7,8,9];
+// const removed = nums.splice(2,5, 77, 78, 90);
+// console.log(removed);
+// console.log(nums);
+
+// const nums = [1,2,3,4,5,6,7,8,9];
+// const removed = nums.splice(2,5, 77, 78, 90);
+// const together = nums.join(",");
+// console.log(together);
+
+const nums = ["morsd", "shapin", "fadu", "momin", "tamim", "kamal", "babu", "dufi"];
+const argentina = nums.slice(0,4);
+const brazil = nums.splice(4, 7);
+console.log(argentina);
+console.log(brazil);
